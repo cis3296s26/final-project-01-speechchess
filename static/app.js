f1fffb17944d67927a676940cee1236e6ccfdc7b
Thompson <tun78919@temple.edu>
2026-03-19 19:05:15 -0400

    Added buttons for login and sign-up. Also added visual hover capabilities for those buttons and the get started button. Still need to write the JavaScript for these buttons.

--- static/app.js
+++ static/app.js
@@ -8,5 +8,15 @@ function toggleSubMenu(button){
 function getStarted(button){
     // Access the next element sibling, which will be the redirection to sign up or log in. They should be the same hierarchy level within the same li element. 
     button.redirectGetStarted
-    window.location.href = "http://www.w3schools.com";
+    window.location.href = "http://www.w3schools.com"
+}
+
+function login(button){
+    button.redirectedLogin
+    window.location.href = "http://www.w3schools.com"
+}
+
+function signUp(button){
+    button.redirectedSignUp
+    window.location.href = "http://www.w3schools.com"
 }
