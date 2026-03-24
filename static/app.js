af28dcf173cc75bce5db5bc8ecca4c2bf432b886
Thompson <tun78919@temple.edu>
2026-03-23 22:19:22 -0400

    Modified file structure. Add new html files for redirection and configured redirection.

--- static/app.js
+++ static/app.js
@@ -6,7 +6,7 @@ function toggleSubMenu(button){
 } 
 
 function getStarted(button){
-    window.location.href = "/getstarted"
+    window.location.href = "/get_started"
 }
 
 function login(button){
@@ -17,17 +17,57 @@ function signUp(button){
     window.location.href = "/signup"
 }
 
+// All the calls for window redirection to the play directory html files
 function play(button){
-    window.location.href="/play"
-
+    window.location.href="play/play"
 }
 
-function learn(button){
-    window.location.href="/learn"
+function playOnline(button){
+    window.location.href="play/play_online"
 }
 
+function playAI(button){
+    window.location.href="play/play_ai"
+}
+
+function playFriends(button){
+    window.location.href="play/play_friends"
+}
+
+function stats(button){
+    window.location.href="play/stats"
+}
+
+function history(button){
+    window.location.href="play/history"
+}
+
+// All the calls for window redirection to the puzzles directory html files
 function puzzles(button){
-    window.location.href="/puzzles"
-
+    window.location.href="puzzles/puzzles"
 }
 
+function puzzlesDaily(button){
+    window.location.href="puzzles/daily_puzzle"
+}
+
+function puzzlesAll(button){
+    window.location.href="puzzles/all_puzzles"
+}
+
+// Rest of the calls for window redirection to the sidebar html files
+function learn(button){
+    window.location.href="sidebar/learn"
+}
+
+function community(button){
+    window.location.href="sidebar/community"
+}
+
+function settings(button){
+    window.location.href="sidebar/settings"
+}
+
+function support(button){
+    window.location.href="sidebar/support"
+}
