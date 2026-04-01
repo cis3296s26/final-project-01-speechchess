2419ff8e017d6e651dc8dd404808a96e80e23d09
Thompson <tun78919@temple.edu>
2026-04-01 12:17:40 -0400

    Fixed the styling of the login page.

--- app.js
+++
@@ -1,6 +0,0 @@
-function toggleSubMenu(button){
-    // Access the next element sibling, which is the submenu. Button and submenu are sibling elements as they're the same hierarchy level within the same li element.
-    button.nextElementSibling.classList.toggle('show')
-    // Rotate the svg arrow from down to up as the submenu is now open.
-    button.classList.toggle('rotate')
-} 
